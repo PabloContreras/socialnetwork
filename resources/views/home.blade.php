110b57c225ad1054c79b2e4b0df4cf50fc800501
pablocontreras <pablo_contreras_1997@outlook.com>
2019-07-19 16:53:33 -0500

    indent

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,42 +48,41 @@
                 </div>
                 {{--@include('auth.partials.feed')--}}
                 <br><br>
-<div class="panel panel-default">
-    <div class="panel-body infinite-scroll">
-        <div class="row">
-            @if( isset($posts) )
-                 @foreach($posts as $post) 
-                    <div class="col-md-12">              
-                        <div class="card border-success mb-3" >
-                            <div class="card-header">
-                                <a href="{{url($post->user_id) }}">
-                                    {{ $post->user->name }} <small> {{ '@' . $post->user->username }} </small>
-                                </a>
-                                <small> &bull; {{ $post->created_at->diffForHumans() }} </small>
-                            </div>
-                            <div class="card-body">
-                                <div class="pull-left">
-                                    <a href="{{ url('/'.$post->user->id) }}">
-                                        <img class="media-object" src="{{ Request::is('tags/*') ? '../' : '' }}uploads/avatars/{{ $post->user->avatar }}" alt="avatar" style="width: 64px; height: 64px;">
-                                    </a>
-                                </div>
-                                <p class="card-text"> <h2><a href="{{ url('posts/'.$post->id) }}"> {!! $post->body !!} </a></h2></p>
-                            </div>
-                            @if( $post->image != 'null')
-                                <center><img class="media-object" src="{{ Request::is('tags/*') ? '../' : '' }}uploads/posts/{{ $post->image }}" alt="avatar" style="width: 50%; height: 50%;"></center>
+                <div class="panel panel-default">
+                    <div class="panel-body infinite-scroll">
+                        <div class="row">
+                            @if( isset($posts) )
+                                 @foreach($posts as $post) 
+                                    <div class="col-md-12">              
+                                        <div class="card border-success mb-3" >
+                                            <div class="card-header">
+                                                <a href="{{url($post->user_id) }}">
+                                                    {{ $post->user->name }} <small> {{ '@' . $post->user->username }} </small>
+                                                </a>
+                                                <small> &bull; {{ $post->created_at->diffForHumans() }} </small>
+                                            </div>
+                                            <div class="card-body">
+                                                <div class="pull-left">
+                                                    <a href="{{ url('/'.$post->user->id) }}">
+                                                        <img class="media-object" src="{{ Request::is('tags/*') ? '../' : '' }}uploads/avatars/{{ $post->user->avatar }}" alt="avatar" style="width: 64px; height: 64px;">
+                                                    </a>
+                                                </div>
+                                                <p class="card-text"> <h2><a href="{{ url('posts/'.$post->id) }}"> {!! $post->body !!} </a></h2></p>
+                                            </div>
+                                            @if( $post->image != 'null')
+                                                <center><img class="media-object" src="{{ Request::is('tags/*') ? '../' : '' }}uploads/posts/{{ $post->image }}" alt="avatar" style="width: 50%; height: 50%;"></center>
+                                            @endif
+                                            <div class="card-footer">
+                                               @include('auth.partials.like')
+                                            </div>
+                                        </div>
+                                    </div> 
+                                @endforeach
+                                {{ $posts->links() }}
                             @endif
-                            <div class="card-footer">
-                               @include('auth.partials.like')
-                            </div>
-                        </div>
-                    </div> 
-                @endforeach
-                {{ $posts->links() }}
-            @endif
-        </div>
-            
-    </div>
-</div>
+                        </div>                            
+                    </div>
+                </div>
             </div>
         </div>
     </div>
